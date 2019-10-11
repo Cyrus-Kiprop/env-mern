2f0191a3b2f2c85bf36371b6d2e36f284c76484c
@cyrusKip <kip@localhost.localdomain>
2019-10-11 18:23:45 +0300

    initial commit

--- src/client/App.js
+++ src/client/App.js
@@ -3,32 +3,11 @@ import './app.css';
 import Card from './card';
 
 const App = () => {
-  const [data, setdata] = useState(null);
-
-  async function fetchProduct() {
-    const response = await fetch('/api/getUsername');
-    const json = await response.json();
-    setdata(json);
-  }
-
-  useEffect(() => {
-    fetchProduct();
-  }, []);
+  // I will do the functionality over here
 
   return (
     <div>
-      {data && data.map((info) => {
-        const { title, date, text, _id } = info;
-
-        return (
-          // eslint-disable-next-line no-underscore-dangle
-          <div key={info._id}>
-            <Card
-              id={_id}
-              title={title}
-              date={date}
-              text={text}
-            />
+      
           </div>
         );
       })
